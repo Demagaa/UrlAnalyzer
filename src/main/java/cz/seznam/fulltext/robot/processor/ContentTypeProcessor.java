726c8a798b862e6e4951d3f6f098f5ec98d6224d
Demaga <alekschursin@email.cz>
2023-10-04 17:28:04 +0200

    Implementing best practices

--- src/main/java/cz/seznam/fulltext/robot/processor/ContentTypeProcessor.java
+++ src/main/java/cz/seznam/fulltext/robot/processor/ContentTypeProcessor.java
@@ -3,32 +3,36 @@ package cz.seznam.fulltext.robot.processor;
 import cz.seznam.fulltext.robot.exception.InvalidFileFormatException;
 import cz.seznam.fulltext.robot.processor.api.Processor;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ContentTypeProcessor implements Processor {
 
-    public void process(String regex) {
+    public void process(String regex) throws InvalidFileFormatException {
         Map<String, Integer> contentTypeCounts = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("\t");
+                if (parts.length != 3) {
+                    // Invalid input line, skipping
+                    continue;
+                }
 
-        String line = "";
-        while (line.isEmpty()) {
-            String[] parts = line.split("\t");
-            if (parts.length != 3) {
-                // Invalid input line, skipping
-                continue;
+                String contentType = parts[1];
+
+                // Update or initialize the count for the content type
+                contentTypeCounts.put(contentType, contentTypeCounts.getOrDefault(contentType, 0) + 1);
             }
 
-            String contentType = parts[1];
-
-            // Update or initialize the count for the content type
-            contentTypeCounts.put(contentType, contentTypeCounts.getOrDefault(contentType, 0) + 1);
+            // Sort and output the content type counts
+            contentTypeCounts.entrySet().stream()
+                    .sorted(Map.Entry.comparingByKey())
+                    .forEach(entry -> System.out.println(entry.getKey() + "\t" + entry.getValue()));
+        } catch (Exception e) {
+            throw new InvalidFileFormatException(e.getMessage());
         }
-
-        // Sort and output the content type counts
-        contentTypeCounts.entrySet().stream()
-                .sorted(Map.Entry.comparingByKey())
-                .forEach(entry -> System.out.println(entry.getKey() + "\t" + entry.getValue()));
-
     }
 }
--- src/main/java/cz/seznam/fulltext/robot/processor/GrepProcessor.java
+++ src/main/java/cz/seznam/fulltext/robot/processor/GrepProcessor.java
@@ -1,23 +1,28 @@
 package cz.seznam.fulltext.robot.processor;
 
+import cz.seznam.fulltext.robot.exception.InvalidFileFormatException;
 import cz.seznam.fulltext.robot.processor.api.Processor;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GrepProcessor implements Processor {
 
-    public void process(String regex) {
-        Scanner scanner = new Scanner(System.in);
+    public void process(String regex) throws InvalidFileFormatException {
 
-        Pattern pattern = Pattern.compile(regex);
-        String line = scanner.nextLine();
-        while (!line.isEmpty()) {
-            Matcher matcher = pattern.matcher(line);
-            if (matcher.find()) {
-                System.out.println(line);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            Pattern pattern = Pattern.compile(regex);
+            String line;
+            while ((line = br.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    System.out.println(line);
+                }
             }
+        } catch (Exception e) {
+            throw new InvalidFileFormatException(e.getMessage());
         }
     }
 }
